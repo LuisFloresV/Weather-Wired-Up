2430118e11566228d2ca25b3575ef196e4239175
Luis Daniel Flores Velazquez <luisfv98@outlook.com>
2021-04-21 23:01:48 -0500

    Removed log

--- public/js/index.js
+++ public/js/index.js
@@ -12,19 +12,17 @@ const elem = document.createElement("img");
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
   const location = search.value
-  console.log(location)
   locationS.textContent = 'Loading...'
   forecast.textContent = ''
   fetch(`weather?address=${location}`)
     .then(response => response.json())
     .then(data => {
-      console.log(data)
       if (data.error) {
         locationS.textContent = `${data.message}`
       } else {
         elem.src = `${data.forecastData.icon}`
         document.getElementById("icon").appendChild(elem);
-        locationS.textContent = `${data.forecastData.description}. It is currently ${data.forecastData.temp} degrees but it feels like ${data.forecastData.feels} degrees.`
+        locationS.textContent = `${data.location}. ${data.forecastData.description}. It is currently ${data.forecastData.temp} degrees but it feels like ${data.forecastData.feels} degrees.`
       }
     })
 })
